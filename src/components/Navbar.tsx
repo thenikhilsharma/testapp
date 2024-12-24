a324a552ce77da54ee0a0f9251a9ab8022b61fa0
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2024-12-24 17:38:40 +0530

    run

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+        <h1>AiMS</h1>
+        <h2>your own academic companion</h2>
+    </div>
+  )
+}
+
+export default Navbar
