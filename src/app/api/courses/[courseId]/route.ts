4f7ef250faff8f1a30fd9e6cda4cf125e98644c6
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2024-12-28 06:11:41 +0530

    deploy vercel

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -1,22 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
-import dbConnect from '@/lib/dbConnect';
-import Course from '@/model/Course';
+import dbConnect from "@/lib/dbConnect";
+import Course from "@/model/Course";
 
-export async function GET(req: NextRequest, context: { params: { courseId: string } }) {
-  const { params } = context; // Access the `params` object first
-  const courseId = params.courseId; // Then access `courseId`
-
+export async function GET(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
   try {
     await dbConnect();
-    const course = await Course.findOne({ courseId });
+
+    const {courseId} = await params;
+    const course = await Course.find({ courseId: courseId });
 
     if (!course) {
-      return NextResponse.json({ error: 'Course not found' }, { status: 404 });
+      return Response.json({ error: "Course not found" }, { status: 404 });
     }
 
-    return NextResponse.json(course, { status: 200 });
+    return Response.json(course, { status: 200 });
   } catch (error) {
-    console.error('Database connection or query error:', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    console.error(error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
