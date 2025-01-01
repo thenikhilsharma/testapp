ab38861749c0811f475505d1013cfd424efe374a
Nikhil Sharma <83294513+thenikhilsharma@users.noreply.github.com>
2025-01-02 02:16:29 +0530

    added signup page + improved css

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,110 @@
+// app/signin/page.js
+"use client";
+
+import { useState } from "react";
+import "./signup.css";
+import SignUpSuccess from "@/components/signUpSuccess";
+
+export default function SignIn() {
+  const [username, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({
+    courseId: "",
+    courseName: "",
+    Attendance: "",
+  });
+  console.log("validationErrors", validationErrors);
+
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+
+    setValidationErrors({
+      courseId: "",
+      courseName: "",
+      Attendance: "",
+    });
+
+    try {
+      const response = await fetch("/api/auth/signUp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.status === 201) {
+        const data = await response.json();
+        console.log("User registered:", data);
+        setIsSubmitted(true);
+      } else if (response.status === 400) {
+        const data = await response.json();
+        console.log("Error data from server:", data); // Log the server error response
+        if (data.errors) {
+          setValidationErrors(data.errors);
+        } else {
+          // Handle unexpected error structure
+          console.error("Unexpected error structure:", data);
+        }
+      } else {
+        console.error("Registration failed:", response.statusText);
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Sign-Up failed. Please check your credentials and try again.");
+    }
+  };
+
+  return (
+    <div>
+      {isSubmitted ? (
+        <SignUpSuccess />
+      ) : (
+        <div className="signupContainer">
+          <div className="signupDiv">
+            <div>
+              <h1 className="signupLabel">Sign UP</h1>
+            </div>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <form
+              onSubmit={handleSubmit}
+              style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+            >
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                style={{ padding: "10px", fontSize: "16px" }}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                style={{ padding: "10px", fontSize: "16px" }}
+              />
+              <button
+                type="submit"
+                style={{
+                  padding: "10px",
+                  fontSize: "16px",
+                  backgroundColor: "#237D8B",
+                  color: "white",
+                  border: "none",
+                  borderRadius: "5px",
+                }}
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -8,14 +8,12 @@ interface CourseCardProps {
 const CourseCard: React.FC<CourseCardProps> = ({ courseName, Attendance }) => {
   return (
     <div className="dashboard-container">
-      <div className="attendance-box">
-        <div className="attendance-content">
-          <div className="attendance-progress">
-            <p>{Attendance}%</p>
-          </div>
-          <div className="courseName">
-            <p>{courseName}</p>
-          </div>
+      <div className="course-box">
+        <div className="course-progress">
+          <p>{Attendance}%</p>
+        </div>
+        <div className="courseName">
+          <p>{courseName}</p>
         </div>
       </div>
     </div>
